1fd6fd5ce86d84b3f03c80b99157c259d37f032f
GingerBeardx <ecryptographer@gmail.com>
2020-05-20 20:01:26 -0600

    removed pdfminer info and uninstalled

--- main.py
+++ main.py
@@ -9,43 +9,6 @@ methodology for each property. A comparison will then be made between the method
 show what matches and what varies from the service contract. The results will then be 
 saved in another excel file.
 """
-import pdfminer
-
-# Open the service agreement PDF
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
-
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 2
-    caching = True
-    pagenos=set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
-
-pdf = convert_pdf_to_txt(
-    'files\wm66 Waterstone at Moorpark RUBS Billing Agreement (2).pdf'
-)
-# print(pdf)
-
 from tabula import read_pdf
 
 df = read_pdf(
